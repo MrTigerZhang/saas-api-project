8f61c8ed7791ecad163b006217e6bcdebcb2ba25
张泰3 <13313239155@163.com>
2025-09-19 17:23:51 +0800

    项目类型

---
+++ saas-common-core/src/main/java/fun/swinner/constant/ItemTypeEnum.java
@@ -0,0 +1,29 @@
+package fun.swinner.constant;
+
+/**
+ * 项目类型枚举
+ * 数据库字段：item_type VARCHAR(20)
+ * 含义：product 商品 / service 服务 / fee 费用 / other 其它
+ */
+public enum ItemTypeEnum {
+    PRODUCT("product", "商品"),
+    SERVICE("service", "服务"),
+    FEE("fee", "费用"),
+    OTHER("other", "其它");
+
+    private final String code;
+    private final String description;
+
+    ItemTypeEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
